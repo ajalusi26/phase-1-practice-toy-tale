93ba9ee7b2d96081ce373aa7b62e0eeae1d87006
AJ Jalusi <ajalusi26@gmail.com>
2021-12-21 12:21:50 -0500

    so far,

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 
-function stuff (){
+function allToyData (){
   fetch('http://localhost:3000/toys')
     .then(res => res.json())
     .then((data) => {
@@ -9,7 +9,32 @@ function stuff (){
   }
 
   let addToy = false;
-stuff()
+function displayToys(toy){
+   let name = document.createElement('p')
+   name.textContent = toy.name
+   
+   let img = document.createElement('img')
+   img.src = toy.image
+   img.className = "toy-avatar"
+   
+   let likes = toy.likes
+   let likeButton = document.createElement('button')
+   likeButton.id = "like"
+   likeButton.textContent = `${likes} likes`
+   
+   let div = document.createElement('div')
+   div.className = "card"
+   div.append(name)
+   div.appendChild(img)
+   div.appendChild(likeButton)
+   document.querySelector("#toy-collection").appendChild(div)
+  //  addLikes(likes, likeButton)
+  }
+  
+allToyData()
+
+
+
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
@@ -24,13 +49,13 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 })
-  
-function displayToys(toy){
-  let div = document.createElement('div')
-  div.className = "card"
-  div.innerHTML = `<h1> ${toy.name}</h1>
-                    <img src = "${toy.image}>"
-                    <p>${toy.likes} likes</p>`
-  document.querySelector("#toy-collection").appendChild(div)
-}
+
+
+// function addLikes(likes, likeButton){
+//   document.querySelector('#like').addEventListener("click", e => {
+//     e.preventDefault()
+//     e.target.likes = likes + 1
+//     e.target.textContent = `${likes} likes`
+//   })
+// }
 
