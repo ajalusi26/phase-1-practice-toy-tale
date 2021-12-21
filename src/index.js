462420d250f40166393dee8421c8929429c299e0
AJ Jalusi <ajalusi26@gmail.com>
2021-12-21 11:10:50 -0500

    so far, this

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
-let addToy = false;
 
+function stuff (){
+  fetch('http://localhost:3000/toys')
+    .then(res => res.json())
+    .then((data) => {
+      console.log(data)
+      data.forEach(displayToys)
+    })
+  }
+
+  let addToy = false;
+stuff()
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
@@ -8,8 +18,19 @@ document.addEventListener("DOMContentLoaded", () => {
     addToy = !addToy;
     if (addToy) {
       toyFormContainer.style.display = "block";
+      newToyForm()
     } else {
       toyFormContainer.style.display = "none";
     }
   });
-});
+})
+  
+function displayToys(toy){
+  let div = document.createElement('div')
+  div.className = "card"
+  div.innerHTML = `<h1> ${toy.name}</h1>
+                    <img src = "${toy.image}>"
+                    <p>${toy.likes} likes</p>`
+  document.querySelector("#toy-collection").appendChild(div)
+}
+
